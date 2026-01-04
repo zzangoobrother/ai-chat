d1cd6029f8c175bb3e806f69572e6ea41f7a5a21
choiseonkang <com1368@naver.com>
2026-01-04 21:36:36 +0900

    feat : 동기/비동기 구현

--- src/main/java/com/example/aichat/rest/SimpleChatController.java
+++ src/main/java/com/example/aichat/rest/SimpleChatController.java
@@ -1,8 +1,11 @@
 package com.example.aichat.rest;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class SimpleChatController {
@@ -18,6 +21,22 @@ public class SimpleChatController {
         return chatClient.prompt()
                 .user(userPrompt)
                 .call()
+                .content();
+    }
+
+    @GetMapping(value = "/call", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ChatResponse call(String userPrompt) {
+        return chatClient.prompt()
+                .user(userPrompt)
+                .call()
+                .chatResponse();
+    }
+
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> stream(String userPrompt) {
+        return chatClient.prompt()
+                .user(userPrompt)
+                .stream()
                 .content();
     }
 }
